f44b33591ade7690d4a5875d06c03f98eb4cd58b
huangtingxiang <2228672279@qq.com>
2018-07-16 13:17:28 +0800

    Merge pull request #14 from mengyunzhi/ctest
    抢课界面原型

---
+++ application/index/controller/Lab.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\index\controller;
+use think\Controller;
+
+class Lab extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+<<<<<<< HEAD
+    public function creatcode()
+    {
+    	return $this->fetch();
+    }
+=======
+>>>>>>> b7bdff894516b42df633c70c3f339ac274004415
+}
