d6e7a86ec2771d0380eccd5c7dc714e7162c6803
huangtingxiang <2228672279@qq.com>
2018-07-16 14:32:55 +0800

    二维码生成

--- application/index/controller/Lab.php
+++ application/index/controller/Lab.php
@@ -8,4 +8,20 @@ class Lab extends Controller
     {
         return $this->fetch();
     }
+    public function creatcode()
+    {
+    	return $this->fetch();
+    }
+    public function changeclasses()
+    {
+    	return $this->fetch();
+    }
+    public function personalinformation()
+    {
+    	return $this->fetch();
+    }
+    public function takelesson()
+    {
+    	return $this->fetch();
+    }
 }
