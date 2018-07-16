18456e3fc49480713ddb7483c88849efa94a3da7
huangtingxiang <2228672279@qq.com>
2018-07-16 18:58:20 +0800

    文件名的修改2
    把文件名下划线去掉，改为c层fetch添加视图文件名

--- application/index/controller/Lab.php
+++ application/index/controller/Lab.php
@@ -10,20 +10,20 @@ class Lab extends Controller
     }
     public function creatCode()
     {
-    	return $this->fetch();
+    	return $this->fetch("creatCode");
     }
 
     public function changeClasses()
     {
-    	return $this->fetch();
+    	return $this->fetch("changeClasses");
     }
     public function personalInformation()
     {
-    	return $this->fetch();
+    	return $this->fetch("personalInformation");
     }
     public function takeLesson()
     {
-    	return $this->fetch();
+    	return $this->fetch("takeLesson");
     }
 
 }
