adf2c10597a54eb4821a906e6fe86fef61e43a61
李宜衡 <33796774+liyiheng1111@users.noreply.github.com>
2018-08-15 17:36:36 +0800

    修正了学生信息的bug
    Closes #87 修改了学生不能正常保存的bug

--- application/index/controller/StudentController.php
+++ application/index/controller/StudentController.php
@@ -3,6 +3,7 @@
 
 namespace app\index\controller;
 
+use app\common\model\Klass;
 use app\common\model\Student;
 use think\Controller;
 use think\exception\HttpResponseException;
@@ -14,36 +15,28 @@ class StudentController extends Controller
 {
     public function student()
     {
+        //没有扫码，因此直接得到学生信息
+        $Student = Student::get('1');
 
-        // 页面的查询功能和分页
-        $name = input('get.name');
+        if(is_null($Student))
+        {
+            return $this->error('不存在这个学生');
+        }
+        $klasses = Klass::all();
 
-        $pageSize = 5;
+        $this->assign('classes',$klasses);
+        $this->assign('Student',$Student);
 
-        $student = new student();
-       //按条件查询数据并调用分页
-        $students = $student->where('name','like','%'.$name.'%')
-            ->paginate($pageSize,false,[
-                'query' =>[
-                 'name' => $name,
-                ]
-                ]);
-
-        //向V层传数据
-        $this->assign('students', $students);
-
-        //渲染数据
-        return $this->fetch();
-
-        //通过扫码得到一个信息，通过这个找到这个学生
         return $this->fetch('student');
+
+
        
     }
 
     //保存数据
     public function save()
     {
-        try{
+
             $id = Request::instance()->post('id');
 
             //判断是否接收成功
@@ -58,30 +51,19 @@ class StudentController extends Controller
             }
 
             $Student->name = Request::instance()->post('name');
-            $Student->class = Request::instance()->post('class');
-            $result = $Student->save();
 
-            if (!$result)
-            {
-            }
+            $Student->klass_id = Request::instance()->post('class');
+            $Student->save();
 
 
             //成功跳转到原页面
             return $this->success('操作成功',url('student'));
 
-            //获取到正常的异常时输出异常
-        }catch (\Exception $exception)
-        {
-            return $exception->getMessage();
-        }catch (HttpResponseException $exception){
-            throw $exception;
-        }
-
     }
 
     public function index()
     {
-        
+        return $this->fetch();
     }
 
     public function administrate()
